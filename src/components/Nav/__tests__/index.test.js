64972c5e6f1e7aaa2fe47ddeee6ed0030342330e
Becca Wyland <rwyland@uwalumni.com>
2021-07-27 22:43:10 -0500

    push

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,17 +3,23 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
+const categories = [
+    { name: 'portraits', description: 'Portraits of people in my life'}
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+
 afterEach(cleanup);
 
 describe('Nav component', () => {
     // baseline test
     it('renders', () => {
-        render(<Nav />);
+        render(<Nav categories={categories} setCurrentCategory={mockSetCurrentCategory} currentCategory={mockCurrentCategory}/>);
     });
 
-    // snapshot test
+    // snapshot test - SOMETHING NOT PASSING HERE?
     it('matches snapshot', () => {
-        const { asFragment } = render(<Nav />);
+        const { asFragment } = render(<Nav categories={categories} setCurrentCategory={mockSetCurrentCategory} currentCategory={mockCurrentCategory}/>);
 
         // assert value comparison
         expect(asFragment()).toMatchSnapshot();
@@ -23,7 +29,7 @@ describe('Nav component', () => {
 describe('emoji is visible', () => {
     it('inserts emoji into the h2', () => {
         // arrange
-        const { getByLabelText } = render(<Nav />);
+        const { getByLabelText } = render(<Nav categories={categories} setCurrentCategory={mockSetCurrentCategory} currentCategory={mockCurrentCategory}/>);
 
         // assert, gets 'camera' from the aria-label on the span that contains the emoji
         expect(getByLabelText('camera')).toHaveTextContent('📸');
@@ -33,7 +39,7 @@ describe('emoji is visible', () => {
 describe('links are visible', () => {
     it('inserts text into the links', () => {
         // arrange
-        const { getByTestId } = render(<Nav />);
+        const { getByTestId } = render(<Nav categories={categories} setCurrentCategory={mockSetCurrentCategory} currentCategory={mockCurrentCategory}/>);
 
         // assert homepage
         expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
